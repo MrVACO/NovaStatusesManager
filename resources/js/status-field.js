30ec1bbf76f1005a580404b8c2c71bd00c2dc9ef
Mr.VACO <brute1904@gmail.com>
2023-06-21 19:24:59 +0500

    Added Form Status field

--- resources/js/status-field.js
+++ resources/js/status-field.js
@@ -1,7 +1,9 @@
 import StatusIndexField from "./components/StatusIndexField";
 import StatusDetailField from "./components/StatusDetailField";
+import StatusFormField from "./components/StatusFormField";
 
 Nova.booting((app, store) => {
     app.component('index-status-field', StatusIndexField)
     app.component('detail-status-field', StatusDetailField)
+    app.component('form-status-field', StatusFormField)
 })
--- src/Fields/Status.php
+++ src/Fields/Status.php
@@ -4,17 +4,22 @@ declare(strict_types = 1);
 
 namespace MrVaco\NovaStatusesManager\Fields;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Stringable;
+use Laravel\Nova\Contracts\FilterableField;
+use Laravel\Nova\Exceptions\NovaException;
 use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\FieldFilterable;
+use Laravel\Nova\Fields\Filters\SelectFilter;
+use Laravel\Nova\Fields\Searchable;
 use Laravel\Nova\Fields\SupportsDependentFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Util;
 use MrVaco\NovaStatusesManager\Models\Statuses;
 
-class Status extends Field
+class Status extends Field implements FilterableField
 {
-    use SupportsDependentFields;
-    
-    public $showOnCreation = false;
-    
-    public $showOnUpdate = false;
+    use SupportsDependentFields, FieldFilterable, Searchable;
     
     public function component(): string
     {
@@ -23,5 +28,140 @@ class Status extends Field
         ]);
         
         return 'status-field';
+    }
+    
+    /**
+     * The field's options callback.
+     *
+     * @var array<string|int, array<string, mixed>|string>|\Closure|callable|\Illuminate\Support\Collection|null
+     *
+     * @phpstan-var TOption|(callable(): (TOption))|(\Closure(): (TOption))|null
+     */
+    public $optionsCallback;
+    
+    /**
+     * Set the options for the select menu.
+     *
+     * @param  array<string|int, array<string, mixed>|string>|\Closure|callable|\Illuminate\Support\Collection  $options
+     *
+     * @return $this
+     *
+     * @phpstan-param TOption|(callable(): (TOption))|(\Closure(): (TOption))                                   $options
+     */
+    public function options($options): static
+    {
+        $this->optionsCallback = $options;
+        
+        return $this;
+    }
+    
+    /**
+     * Display values using their corresponding specified labels.
+     *
+     * @return $this
+     */
+    public function displayUsingLabels(): static
+    {
+        $this->displayUsing(function($value)
+        {
+            if (is_null($value) || $this->isValidNullValue($value))
+            {
+                return $value;
+            }
+            
+            return collect($this->serializeOptions(false))
+                       ->where('value', $value)
+                       ->first()['label'] ?? $value;
+        });
+        
+        return $this;
+    }
+    
+    /**
+     * Enable subtitles within the related search results.
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function withSubtitles(): static
+    {
+        throw NovaException::helperNotSupported(__METHOD__, __CLASS__);
+    }
+    
+    /**
+     * Prepare the field for JSON serialization.
+     *
+     * @return array
+     */
+    public function serializeForFilter(): array
+    {
+        return transform($this->jsonSerialize(), function($field)
+        {
+            return Arr::only($field, [
+                'uniqueKey',
+                'name',
+                'attribute',
+                'options',
+                'searchable',
+            ]);
+        });
+    }
+    
+    /**
+     * Serialize options for the field.
+     *
+     * @param  bool  $searchable
+     *
+     * @return array<int, array<string, mixed>>
+     *
+     * @phpstan-return array<int, array{group: string, label: string, value: TOptionValue}>
+     */
+    protected function serializeOptions($searchable): array
+    {
+        /** @var TOption $options */
+        $options = value($this->optionsCallback);
+        
+        if (is_callable($options))
+        {
+            $options = $options();
+        }
+        
+        return collect($options ?? [])->map(function($label, $value) use ($searchable)
+        {
+            $label = $label instanceof Stringable ? (string) $label : $label;
+            $value = Util::safeInt($value);
+            
+            if ($searchable && isset($label['group']))
+            {
+                return [
+                    'label' => $label['group'] . ' - ' . $label['label'],
+                    'value' => $value,
+                ];
+            }
+            
+            return is_array($label) ? $label + ['value' => $value] : ['label' => $label, 'value' => $value];
+        })->values()->all();
+    }
+    
+    /**
+     * Prepare the field for JSON serialization.
+     *
+     * @return array<string, mixed>
+     */
+    public function jsonSerialize(): array
+    {
+        $this->withMeta([
+            'options' => $this->serializeOptions($searchable = $this->isSearchable(app(NovaRequest::class))),
+        ]);
+        
+        return array_merge(parent::jsonSerialize(), [
+            'searchable' => $searchable,
+        ]);
+    }
+    
+    protected function makeFilter(NovaRequest $request): SelectFilter
+    {
+        return new SelectFilter($this);
     }
 }
